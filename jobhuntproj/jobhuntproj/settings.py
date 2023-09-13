a14633f532529d209eaf8cf0ee5aab5e82da10cc
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-09-13 13:17:38 -0600

    connected front and back

--- jobhuntproj/jobhuntproj/settings.py
+++ jobhuntproj/jobhuntproj/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobhunt'
 ]
 
 MIDDLEWARE = [
@@ -121,7 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [ BASE_DIR / 'static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTH_USER_MODEL = 'jobhunt.App_User' ACTIVATE THIS***
