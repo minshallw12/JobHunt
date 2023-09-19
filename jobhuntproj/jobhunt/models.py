11c17fc71d8c9ceceb15b24d8dfa5754cce84e21
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-09-19 14:56:45 -0600

    Created database models

--- jobhuntproj/jobhunt/models.py
+++ jobhuntproj/jobhunt/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class App_User(AbstractUser):
+    email = models.EmailField(blank=False, null=False, unique=True, max_length=254)
+    name = models.CharField(null = False, blank= False, max_length=50)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"{self.name} | {self.email}"
+    
+class Jobs (models.Model):
+    company = models.CharField(blank=False, null=False, max_length=254)
+    role = models.CharField(blank=False, null=False, max_length=254)
+    date_applied = models.DateField(blank=False)
+    followed_up = models.BigIntegerField()
+    rejected = models.CharField(max_length=254)
+    req_number = models.CharField(max_length=254)
+    recruiter = models.CharField(max_length=254)
+    recruiter_email = models.EmailField(max_length=254)
+    referral = models.CharField(max_length=254)
+    referral_email = models.EmailField(max_length=254)
+    user = models.ForeignKey(App_User, on_delete=models.CASCADE, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.company} | {self.role} | Dunder"
+
+class Interviews (models.Model):
+    job = models.ForeignKey(Jobs, on_delete=models.CASCADE, null=True)
+    offer = models.BooleanField(default=False)
+    notes = models.CharField(max_length=254)
+
+    def __str__(self) -> str:
+        return f"{self.job} | Dunder"
--- jobhuntproj/jobhuntproj/settings.py
+++ jobhuntproj/jobhuntproj/settings.py
@@ -129,4 +129,4 @@ STATICFILES_DIRS = [ BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'jobhunt.App_User' ACTIVATE THIS***
+AUTH_USER_MODEL = 'jobhunt.App_User'
